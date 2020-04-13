ae3415d994717f2c0156646b8a8f55d12a4e195a
te25son <te25son@gmail.com>
2020-04-13 20:30:14 +0200

    Demonstrate use of lists

--- generic_collections/Program.cs
+++ generic_collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace generic_collections
 {
@@ -6,7 +7,10 @@ namespace generic_collections
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("---- ARRAYS ----");
             UsingArrays();
+            System.Console.WriteLine("---- LISTS ----");
+            UsingLists();
         }
 
         private static void UsingArrays()
@@ -29,7 +33,49 @@ namespace generic_collections
                 System.Console.WriteLine(employees[i].Name);
             }
 
+            // Arrays can be resized if needed, but this is considered bad practice.
+            // It is better to use lists if you're unsure the exact length of your array,
+            // or know you will want to add or remove items from it.
             Array.Resize(ref employees, 10);
+        }
+
+        private static void UsingLists()
+        {
+            // Demonstartes the basic usage of a list object. 
+
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { Name = "Timothy" },
+                new Employee { Name = "Thomas" }
+            };
+
+            // Unlike arrays, lists can be added to.
+            employees.Add(new Employee { Name = "Johnny" });
+
+            foreach (var employee in employees)
+            {
+                System.Console.WriteLine(employee.Name);
+            }
+
+            for (int i = 0; i < employees.Count; i++)
+            {
+                System.Console.WriteLine(employees[i].Name);
+            }
+
+            var numbers = new List<int>();
+            var capacity = -1;
+            var count = 0;
+
+            while (count < 500)
+            {
+                if (numbers.Capacity != capacity)
+                {
+                    capacity = numbers.Capacity;
+                    System.Console.WriteLine(capacity);
+                }
+                numbers.Add(1);
+                count ++;
+            }
         }
     }
 }
