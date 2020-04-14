b7d2e8695ae0b717f99d896cef0c2b0c98e37d0f
te25son <te25son@gmail.com>
2020-04-14 09:20:11 +0200

    Add tests for lists

--- GenericCollections/test/GenericCollections.Tests/UnitTest1.cs
+++ GenericCollections/test/GenericCollections.Tests/UnitTest1.cs
@@ -1,18 +1,55 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace GenericCollections.Tests
 {
-    public class UnitTest1
+    public class ListTests
     {
         [Fact]
-        public void Test1()
+        public void ListCanInsert()
         {
-            List<int> integers = new List<int> { 1, 2, 3 };
+            var integers = new List<int> { 1, 2, 3 };
             integers.Insert(1, 6);
 
             Assert.Equal(6, integers[1]);
+        }
+
+        [Fact]
+        public void ListCanRemove()
+        {
+            var integers = new List<int> { 1, 2, 3, 2 };
+            integers.Remove(2);  // Remove all removes the first instance of the element from a list.
+
+            Assert.True(integers.SequenceEqual(new [] { 1, 3, 2 }));
+        }
+
+        [Fact]
+        public void ListCanRemoveMoreThanOne()
+        {
+            var integers = new List<int> { 1, 2, 3, 2 };
+            integers.RemoveAll(n => n.Equals(2));  // RemoveAll removes all instances of an element from a list. 
+
+            Assert.True(integers.SequenceEqual(new [] { 1, 3 }));
+        }
+
+        [Fact]
+        public void ListCanFindObjects()
+        {
+            var integers = new List<int> { 1, 2, 3 };
+            var index = integers.IndexOf(3);  // Gets the index of the element: 3.
+
+            Assert.Equal(2, index);
+        }
+
+        [Fact]
+        public void ListReturnsDefaultIfObjectDoesNotExist()
+        {
+            var integers = new List<int> { 1, 2, 3 };
+            var index = integers.IndexOf(4);
+
+            Assert.Equal(-1, index);
         }
     }
 }
