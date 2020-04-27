a2fdf4794b8d87b688fd129956d6a3eeeae05092
te25son <te25son@gmail.com>
2020-04-27 08:23:14 +0200

    Add database and model classes

---
+++ QueryIt/DataAccess.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace QueryIt
+{
+    public class EmployeeDb : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public interface IRepository<T> : IDisposable
+    {
+        void Add(T newEntity);
+        void Delete(T entity);
+        void FindById(int Id);
+        IQueryable<T> FindAll();
+        int Commit();
+    }
+
+    public class SqlRepository<T> : IRepository<T>
+        where T : class
+    {
+        DbContext _context;
+        DbSet<T> _set;
+
+        public SqlRepository(DbContext context)
+        {
+            _context = context;
+            _set = _context.Set<T>();
+        }
+
+        public void Add(T newEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Commit()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Dispose();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FindById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QueryIt/Model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryIt
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public virtual void DoWork()
+        {
+            Console.WriteLine("Working");
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public override void DoWork()
+        {
+            Console.WriteLine("Create a meeting");
+        }
+    }
+}
