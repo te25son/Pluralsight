603e50cc3835d86f0e32aaf0ab65928e12349e68
te25son <te25son@gmail.com>
2020-05-03 19:50:16 +0200

    Instancing generic types

--- Reflectit/Program.cs
+++ Reflectit/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ReflectIt
 {
@@ -6,7 +7,25 @@ namespace ReflectIt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var employeeList = CreateCollection(typeof(List<>), typeof(Employee));  // Unbound generic : List<>.
+            var genericArguments = employeeList.GetType().GenericTypeArguments;
+
+            Console.Write(employeeList.GetType().Name);
+            Array.ForEach(
+                genericArguments,
+                a => Console.WriteLine($"[{a.Name}]")
+            );
         }
+
+        private static object CreateCollection(Type collectionType, Type itemType)
+        {
+            var closedType = collectionType.MakeGenericType(itemType);
+            return Activator.CreateInstance(closedType);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
     }
 }
