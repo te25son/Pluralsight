50370a79bd52a557cc8f0374ad3e52cfbd8477d1
te25son <te25son@gmail.com>
2020-05-03 18:48:51 +0200

    Add ReflectIt solution and project

---
+++ Reflectit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReflectIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
