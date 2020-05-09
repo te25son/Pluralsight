c450caef0a5e7dc51529fd2a7000e6248061ae5e
te25son <te25son@gmail.com>
2020-05-09 15:49:13 +0200

    Add services

---
+++ ReflectIt/Services.cs
@@ -0,0 +1,32 @@
+namespace ReflectIt
+{
+    public interface ILogger
+    {
+
+    }
+
+    public class SqlServerLogger : ILogger
+    {
+
+    }
+
+    public interface IRepository<T>
+    {
+
+    }
+
+    public class SqlRepository<T> : IRepository<T>
+    {
+        public SqlRepository(ILogger logger)
+        {
+
+        }
+    }
+
+    public class InvoiceService
+    {
+        public InvoiceService(IRepository<Employee> repository, ILogger logger)
+        {
+        }
+    }
+}
