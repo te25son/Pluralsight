d367bc3c78e6f56fc83f7f4799b4dedb442f9687
te25son <te25son@gmail.com>
2020-05-11 10:12:57 +0200

    Problem with generics and enums

---
+++ OddsAndEnds/Tips/EnumsAndGenerics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tips
+{
+    public enum Steps
+    {
+        Step1,
+        Step2,
+        Step3
+    }
+
+    public static class StringExtensions
+    {
+        public static TEnum ParseEnum<TEnum>(this string value)
+            where TEnum : struct
+            // Using struct here will not eliminate all possible problems.
+            // User can still pass another value type such as 'int' and it will be legal, but won't compile.
+        {
+            return (TEnum)Enum.Parse(typeof(TEnum), value);
+        }
+    }
+
+    public class EnumsAndGenerics
+    {
+        public void Examples()
+        {
+            var input = "Step1";
+            var value = input.ParseEnum<Steps>();
+
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ OddsAndEnds/Tips/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var enumAndGenericExamples = new EnumsAndGenerics();
+            enumAndGenericExamples.Examples();
+        }
+    }
+}
