9463f42c3019308da89d905f02af9c67bc3ce622
te25son <te25son@gmail.com>
2020-05-11 20:33:16 +0200

    Create LinqSamples solution and examples for IEnumerable and IEnumerator

---
+++ linq/Features/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Dexter" },
+                new Employee { Id = 1, Name = "Sherlock" }
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>
+            {
+                new Employee { Id = 3, Name = "Brad" }
+            };
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+        }
+    }
+}
