7766b9c134ade87a7ae161bfec0838dec7242184
te25son <te25son@gmail.com>
2020-05-14 08:21:32 +0200

    Show pitfalls of using deferred execution

--- linq/Queries/Program.cs
+++ linq/Queries/Program.cs
@@ -6,19 +6,30 @@ namespace Queries
 {
     class Program
     {
+        public static List<Movie> _movies = new List<Movie>
+        {
+            new Movie { Title = "The Dark Knight", Rating = 8.9f, Year = 2008 },
+            new Movie { Title = "The King's Speech", Rating = 8.0f, Year = 2010 },
+            new Movie { Title = "Casablanca", Rating = 8.5f, Year = 1942 },
+            new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980 }
+        };
+
         static void Main(string[] args)
         {
-            var movies = new List<Movie>
-            {
-                new Movie { Title = "The Dark Knight", Rating = 8.9f, Year = 2008 },
-                new Movie { Title = "The King's Speech", Rating = 8.0f, Year = 2010 },
-                new Movie { Title = "Casablanca", Rating = 8.5f, Year = 1942 },
-                new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980 }
-            };
+            DeferredExecutionPitfall();
+        }
 
-            var query = movies.Filter(m => m.Year >= 2000).Take(1);
+        public static void DeferredExecutionPitfall()
+        {
+            var query = _movies.Filter(m => m.Year >= 2000);
+            //var query = _movies.Filter(m => m.Year >= 2000).ToList();
             var enumerator = query.GetEnumerator();
-            
+
+            Console.WriteLine(query.Count());  // This will cause the query to run twice.
+                                               // Once to get the count, and again during the while loop below.
+                                               // To avaoid this pitfall, call .ToList or ToArray at the end of your query.
+                                               // This will trigger the query once so you can use it later.
+
             while (enumerator.MoveNext())
             {
                 Console.WriteLine(enumerator.Current.Title);
