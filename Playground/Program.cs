440617687b42f3146e524a9f21bb0605a032682a
te25son <te25son@gmail.com>
2020-05-15 08:55:19 +0200

    Play around with builtin delegates

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Playground
 {
@@ -6,7 +7,45 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var study = new Study();
+            var results = new List<int>
+            {
+                // Uses the same method but returns a different result.
+
+                study.MultiplyByActionResult(2, (x, y) => x + y),  // Multiply 2 by (2 + 2)
+                study.MultiplyByActionResult(2, (x, y) => x - y),  // Multiply 2 by (2 - 2)
+                study.MultiplyByActionResult(2, (x, y) => x * y),  // Multiply 2 by (2 * 2)
+                study.MultiplyByActionResult(2, (x, y) => x / y),  // Multiply 2 by (2 / 2)
+            };
+
+            WriteCollection(results);
+            WriteCollectionCompared(results, x => x > 5);  // Write the items of the collection if the item is greater than 5.
+            WriteCollectionCompared(results, x => x < 5);  // Write the items of the collection if the item is less than 5.
+        }
+
+        public static void WriteCollection<T>(List<T> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void WriteCollectionCompared<T>(List<T> list, Predicate<T> expression)
+        {
+            // Iterate through the list.
+            foreach (var item in list)
+            {
+                // Check if the item in the list matches the expression.
+                //
+                // This expression is a method that takes a single parameter
+                // and returns true or false.
+                if (expression(item))
+                {
+                    // Write the item to the console.
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
---
+++ Playground/Study.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playground
+{
+    public class Study
+    {
+        public int MultiplyByActionResult(int number, Func<int, int, int> action)
+        {
+            // Takes an int and returns that int multiplied by an action
+            // that is determined during use.
+            return number * action(number, number);
+        }
+
+        public int SquareAndMultiplyByActionResult(int number, Func<int, int, int> action)
+        {
+            var squared = number * number;
+            return squared * action(number, squared);
+        }
+    }
+}
