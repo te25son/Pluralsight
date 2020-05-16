10cd19cfa56777a6989fcc90d2e53f49dbc00ec8
te25son <te25son@gmail.com>
2020-05-16 08:37:32 +0200

    Set up cars project

---
+++ linq/Cars/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        
+        public string Manufacturer { get; set; }
+        
+        public string Name { get; set; }
+        
+        public double Dispacement { get; set; }
+        
+        public int Cylinders { get; set; }
+        
+        public double City { get; set; }
+        
+        public int Highway { get; set; }
+        
+        public int Combined { get; set; }
+    }
+}
---
+++ linq/Cars/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
