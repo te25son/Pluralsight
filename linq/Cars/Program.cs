6f77ee22daec0ace91f7ab65046601df75f02a87
te25son <te25son@gmail.com>
2020-05-18 08:34:59 +0200

    Fiter with Where and First

--- linq/Cars/Program.cs
+++ linq/Cars/Program.cs
@@ -16,6 +16,25 @@ namespace Cars
             {
                 Console.WriteLine($"{car.Name} : {car.Combined}");
             }
+
+            FilterBySpecificManufacturer();
+        }
+
+        public static void FilterBySpecificManufacturer()
+        {
+            var processor = new CarCsvProcessor("fuel.csv");
+            var query = processor.List.Where(c => c.Manufacturer.Equals("BMW") && c.Year.Equals(2016))
+                                      .OrderByDescending(c => c.Combined)
+                                      .ThenBy(c => c.Name);
+
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
+            }
+
+            Console.WriteLine(query.First().Name);  // Gets the first element of the sequence.
+            Console.WriteLine(query.FirstOrDefault().Name);  // Gets the first element of the sequence or returns a default value for the type if the sequence is empty.
+
         }
     }
 }
