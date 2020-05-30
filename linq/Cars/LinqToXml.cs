9bc616a06edd990b59632c38b36d047de53f2430
te25son <te25son@gmail.com>
2020-05-30 11:20:58 +0200

    Use linq to get content of xml

--- linq/Cars/LinqToXml.cs
+++ linq/Cars/LinqToXml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 
@@ -13,19 +14,16 @@ namespace Cars
         {
             var records = Processor.ProcessCarsFromCsv("fuel.csv");
             var document = new XDocument();
-            var cars = new XElement("Cars");
-
-            foreach (var record in records)
-            {
-                var car = new XElement("Car");
-                var name = new XElement("Name", record.Name);
-                var combined = new XElement("Combined", record.Combined);
-
-                car.Add(name);
-                car.Add(combined);
-
-                cars.Add(car);
-            }
+            var cars = new XElement("Cars", records.Select(r =>
+                {
+                    return new XElement(
+                        "Car",
+                        new XAttribute("Name", r.Name),
+                        new XAttribute("Combined", r.Combined),
+                        new XAttribute("Manufacturer", r.Manufacturer)
+                    );
+                })
+            );
 
             document.Add(cars);
             document.Save("Fuel.xml");
