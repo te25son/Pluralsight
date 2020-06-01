be15fe3dd88f0029ce2bd8c81d76a16e6790cf72
te25son <te25son@gmail.com>
2020-06-01 08:15:31 +0200

    Query and Create xml with namespaces

--- linq/Cars/LinqToXml.cs
+++ linq/Cars/LinqToXml.cs
@@ -13,11 +13,13 @@ namespace Cars
         public void CreateXml()
         {
             var records = Processor.ProcessCarsFromCsv("fuel.csv");
+            var mainNamespace = (XNamespace)"http://pluralsight.com/cars/2016";
+            var extendedNamespace = (XNamespace)"http://pluralsight.com/cars/2016/ex";
             var document = new XDocument();
-            var cars = new XElement("Cars", records.Select(r =>
+            var cars = new XElement(mainNamespace + "Cars", records.Select(r =>
                 {
                     return new XElement(
-                        "Car",
+                        extendedNamespace + "Car",
                         new XAttribute("Name", r.Name),
                         new XAttribute("Combined", r.Combined),
                         new XAttribute("Manufacturer", r.Manufacturer)
@@ -25,15 +27,19 @@ namespace Cars
                 })
             );
 
+            cars.Add(new XAttribute(XNamespace.Xmlns + "ExtendedNamespace", extendedNamespace));
+
             document.Add(cars);
             document.Save("Fuel.xml");
         }
 
         public void QueryXml()
         {
+            var mainNamespace = (XNamespace)"http://pluralsight.com/cars/2016";
+            var extendedNamespace = (XNamespace)"http://pluralsight.com/cars/2016/ex";
             var document = XDocument.Load("fuel.xml");
             var query =
-                from element in document.Element("Cars").Elements("Car")
+                from element in document.Element(mainNamespace + "Cars").Elements(extendedNamespace + "Car")
                 where element.Attribute("Manufacturer").Value.Equals("BMW")
                 select element.Attribute("Name").Value;
 
