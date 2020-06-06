80703e3692893cabe45404029e9c5f78a63e318c
te25son <te25son@gmail.com>
2020-06-06 09:34:53 +0200

    Add basic examples for arithmetic with date and time

--- DateAndTime/Arithmetic/Arithmetic/Program.cs
+++ DateAndTime/Arithmetic/Arithmetic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Arithmetic
 {
@@ -6,7 +7,18 @@ namespace Arithmetic
     {
         static void Main(string[] args)
         {
-            
+            var timeSpan = new TimeSpan(60, 100, 200);
+
+            Console.WriteLine(timeSpan.Days);
+            Console.WriteLine(timeSpan.Hours);
+            Console.WriteLine(timeSpan.Minutes);
+            Console.WriteLine(timeSpan.Seconds);
+
+            var start = DateTimeOffset.UtcNow;
+            var end = start.AddSeconds(45);
+            var difference = end - start;
+
+            Console.WriteLine(difference.TotalMinutes);
         }
     }
 }
