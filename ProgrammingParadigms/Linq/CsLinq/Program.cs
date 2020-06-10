7d1105799513beece688b84fd61f98a39f1f298e
te25son <te25son@gmail.com>
2020-06-10 08:14:08 +0200

    Set up Programming Paradigms project

---
+++ ProgrammingParadigms/Linq/CsLinq/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CsLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> cities = new[] { "Ghent", "London", "Las Vegas", "Hyderabad" };
+        }
+    }
+}
