b0a0d0cdc8ae84e7dbd6f95611a7a3c54f8bcc9e
te25son <te25son@gmail.com>
2020-06-10 11:05:13 +0200

    Extension methods

--- ProgrammingParadigms/Linq/CsLinq/Program.cs
+++ ProgrammingParadigms/Linq/CsLinq/Program.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 
 namespace CsLinq
 {
+    // Extension Method:
+    // Method that looks like it's a method of a given type,
+    // but in reality it's just a static method of a different type.
+
     class Program
     {
         static void Main(string[] args)
         {
-            IEnumerable<string> cities = new[] { "Ghent", "London", "Las Vegas", "Hyderabad" };
+            var date = new DateTime(2002, 8, 9);
+            var daysTillEndOfMonth = date.DaysToEndOfMonth();
+
+            Console.WriteLine(daysTillEndOfMonth);
+        }
+    }
+
+    public static class DateUtilities
+    {
+        // The 'this' keyword makes this an extension method
+        // of a DateTime object.
+        public static int DaysToEndOfMonth(this DateTime date)
+        {
+            return DateTime.DaysInMonth(date.Year, date.Month) - date.Day;
         }
     }
 }
