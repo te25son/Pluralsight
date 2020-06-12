51f02d06873882047a2a8c582a31342b4c4d7a5d
te25son <te25son@gmail.com>
2020-06-12 11:58:47 +0200

    First dynamic example

---
+++ ProgrammingParadigms/Linq/CsDlr/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsDlr
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public void Speak()
+        {
+            Console.WriteLine($"Hi, my name is {FirstName}.");
+        }
+    }
+}
--- ProgrammingParadigms/Linq/CsDlr/Program.cs
+++ ProgrammingParadigms/Linq/CsDlr/Program.cs
@@ -9,7 +9,17 @@ namespace CsDlr
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            object speaker = GetASpeaker();
+            speaker.GetType().GetMethod("Speak").Invoke(speaker, null);
+
+            // dynamic skips the compile and runs at runtime.
+            dynamic speaker2 = GetASpeaker();
+            speaker2.Speak();
+        }  
+
+        private static object GetASpeaker()
+        {
+            return new Employee { FirstName = "George" };
         }
     }
 }
