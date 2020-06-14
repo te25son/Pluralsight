4655f9b511145fbc32fc8aad9bf4f9e26108360d
te25son <te25son@gmail.com>
2020-06-14 12:37:08 +0200

    Encapsulate code example

--- ProgrammingParadigms/ObjectOrientedProgramming/CsOop/Program.cs
+++ ProgrammingParadigms/ObjectOrientedProgramming/CsOop/Program.cs
@@ -13,38 +13,14 @@ namespace CsOop
         // Polymorphism
         static void Main(string[] args)
         {
-            var date = new List<DateTime>();
-            var open = new List<decimal>();
-            var high = new List<decimal>();
-            var low = new List<decimal>();
-            var close = new List<decimal>();
+            // The code has been abstracted (encapsulation) so that you do not have to understand the intricacies
+            // of the underlying code.
+            // This is similar to a driver getting into a car without understanding exactly how
+            // the engine operates.
 
-            var lines = File.ReadAllLines("msft.csv");
-            for (int i = 1; i < lines.Length; i++)
-            {
-                var data = lines[i].Split(',');
-                date.Add(DateTime.Parse(data[0], CultureInfo.InvariantCulture));
-                open.Add(decimal.Parse(data[1]));
-                high.Add(decimal.Parse(data[2]));
-                low.Add(decimal.Parse(data[3]));
-                close.Add(decimal.Parse(data[4]));
-            }
-
-            for (int i = 0; i < date.Count - 1; i++)
-            {
-                if (open[i] > high[i + 1] && close[i] < low[i + 1])
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"Pivot downside {date[i].ToShortDateString()}");
-                }
-                if (open[i] < low[i + 1] && close[i] > high[i + 1])
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"Pivot upside {date[i].ToShortDateString()}");
-                }
-            }
-
-            Console.ForegroundColor = ConsoleColor.White;
+            var analyzer = new StockQuoteAnalyzer("msft.csv");
+            analyzer.LoadQuotes();
+            analyzer.AnalyzeQuotes();
         }
     }
 }
---
+++ ProgrammingParadigms/ObjectOrientedProgramming/CsOop/StockQuoteAnalyzer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace CsOop
+{
+    public class StockQuoteAnalyzer
+    {
+        readonly string FilePath;
+        readonly List<DateTime> Dates = new List<DateTime>();
+        readonly List<decimal> Opens = new List<decimal>();
+        readonly List<decimal> Highs = new List<decimal>();
+        readonly List<decimal> Lows = new List<decimal>();
+        readonly List<decimal> Closes = new List<decimal>();
+
+        public StockQuoteAnalyzer(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public void LoadQuotes()
+        {
+            var lines = File.ReadAllLines(FilePath);
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var data = lines[i].Split(',');
+                Dates.Add(DateTime.Parse(data[0], CultureInfo.InvariantCulture));
+                Opens.Add(decimal.Parse(data[1]));
+                Highs.Add(decimal.Parse(data[2]));
+                Lows.Add(decimal.Parse(data[3]));
+                Closes.Add(decimal.Parse(data[4]));
+            }
+        }
+
+        public void AnalyzeQuotes()
+        {
+            for (int i = 0; i < Dates.Count - 1; i++)
+            {
+                if (Opens[i] > Highs[i + 1] && Closes[i] < Lows[i + 1])
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Pivot downside {Dates[i].ToShortDateString()}");
+                }
+                if (Opens[i] < Lows[i + 1] && Closes[i] > Highs[i + 1])
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Pivot upside {Dates[i].ToShortDateString()}");
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
