1f4642d6d30b11699008584f9eccb89c1c0387a7
te25son <te25son@gmail.com>
2020-06-19 12:50:22 +0200

    Intro to Curry methods

--- ProgrammingParadigms/FunctionalProgramming/CsFunc/Extensions.cs
+++ ProgrammingParadigms/FunctionalProgramming/CsFunc/Extensions.cs
@@ -28,5 +28,25 @@ namespace CsFunc
 
             return result;
         }
+
+        // Partial function application
+        // Allows you to transform a function that takes `n` parameters
+        // to a function that takes `n - 1` parameters.
+        public static Func<TResult> Partial<TParam1, TResult>(
+            this Func<TParam1, TResult> func, TParam1 parameter)
+        {
+            return () => func(parameter);
+        }
+
+        // Currying
+        // Transform a function that takes n parameters
+        // into a function that you invoke to apply a parameter,
+        // and you get back as a result of function that takes
+        // n - 1 parameters.
+        public static Func<TParam1, Func<TResult>> Curry<TParam1, TResult>(
+            this Func<TParam1, TResult> func)
+        {
+            return parameter => () => func(parameter);
+        }
     }
 }
--- ProgrammingParadigms/FunctionalProgramming/CsFunc/Program.cs
+++ ProgrammingParadigms/FunctionalProgramming/CsFunc/Program.cs
@@ -32,10 +32,13 @@ namespace CsFunc
             //});
             //Console.WriteLine(elapsed);
 
+            var url = "http://microsoft.com";
             var client = new WebClient();
-            Func<string> download = () => client.DownloadString("http://microsoft.com");
+            Func<string, string> download = url => client.DownloadString(url);
+            Func<string, Func<string>> downloadCurry = download.Curry();
 
-            var data = download.WithRetry();
+            var data = download.Partial(url).WithRetry();
+            var data2 = downloadCurry(url).WithRetry();
         }
 
         private static IEnumerable<int> GetRandomNumbers()
@@ -78,6 +81,33 @@ namespace CsFunc
         private static bool IsEven(this int number)
         {
             return number % 2 == 0;
+        }
+
+        private static void MoreCurry()
+        {
+            // An anonymous delegate that takes two ints and returns an int.
+            Func<int, int, int> add = (x, y) => x + y;
+            int a = add(2, 3);
+            Console.WriteLine(a);
+
+            // An anonymous delegate that takes an int and returns an anonymous delegate
+            // that also takes and int and returns an int.
+            Func<int, Func<int, int>> curriedAdd = x => y => x + y;
+            int b = curriedAdd(2)(3);
+
+            // Creates an anonymous delegate that takes an int and adds 5 to it.
+            var add5 = curriedAdd(5);
+            Console.WriteLine(add5(3));
+            Console.WriteLine(add5(2));
+        }
+
+        public static Func<string> SelectorBuild(string value)
+        {
+            
+        }
+
+        public static Func<string, Func<string>> SelectorBuilder(Func<string> func)
+        {
         }
     }
 }
