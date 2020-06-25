8292ba5eb5a4efec3033895eb6cbc018778a06d5
te25son <te25son@gmail.com>
2020-06-25 17:44:13 +0200

    Add examples of expressions vs statements

---
+++ FunctionalProgrammingCSharp/WhatIsFP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WhatIsFP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var posExample = 2;
+            var negExample = -2;
+
+            GetPosOrNegStatement(posExample);
+            GetPosOrNegStatement(negExample);
+
+            GetPosOrNegExpression(posExample);
+            GetPosOrNegExpression(negExample);
+        }
+
+        static void GetPosOrNegStatement(int value)
+        {
+            // posOrNeg is an extraneous variable.
+            string posOrNeg;
+
+            if (value > 0)
+                posOrNeg = "positive";
+            else
+                posOrNeg = "negative";
+
+            Console.WriteLine($"{value} is {posOrNeg}");
+        }
+
+        static void GetPosOrNegExpression(int value)
+        {
+            // Remove the extraneous variable with an expression.
+            Console.WriteLine($"{value} is {(value > 0 ? "positive" : "negative")}");
+        }
+    }
+}
