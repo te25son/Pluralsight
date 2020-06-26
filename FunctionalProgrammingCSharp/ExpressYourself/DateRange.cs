1b0caa740564dfd3f71af884818410681e2913f3
te25son <te25son@gmail.com>
2020-06-26 08:51:38 +0200

    Why immutability matters

---
+++ FunctionalProgrammingCSharp/ExpressYourself/DateRange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressYourself
+{
+    public class DateRange
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public bool DateIsInRange(DateTime checkDate)
+        {
+            return Start.CompareTo(checkDate) <= 0 && End.CompareTo(checkDate) >= 0;
+        }
+    }
+}
---
+++ FunctionalProgrammingCSharp/ExpressYourself/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+
+namespace ExpressYourself
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WhyImmutibilityMatters();
+        }
+
+        static void WhyImmutibilityMatters()
+        {
+            var testDates =
+                new List<DateTime>
+                {
+                    DateTime.Parse("2015-11-03"),
+                    DateTime.Parse("2015-11-01"),
+                    DateTime.Parse("2015-10-01"),
+                    DateTime.Parse("2015-12-01"),
+                };
+
+            var range = new DateRange 
+            { 
+                Start = DateTime.Parse("2015-11-01"),
+                End = DateTime.Parse("2015-11-06") 
+            };
+
+            testDates.ForEach(d => Console.WriteLine($"{d:yyy-MM-dd} - {range.DateIsInRange(d)}"));
+            
+            // The end date of our mutable type is changed, and the result of the line above changes.
+            range.End = DateTime.MaxValue;
+
+            testDates.ForEach(d => Console.WriteLine($"{d:yyy-MM-dd} - {range.DateIsInRange(d)}"));
+        }
+    }
+}
