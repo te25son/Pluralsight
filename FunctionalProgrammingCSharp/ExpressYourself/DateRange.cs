3276c518b8230387d2a78561eaebe5bf7058884f
te25son <te25son@gmail.com>
2020-06-27 08:18:19 +0200

    Use auto getter property in DateRange class

--- FunctionalProgrammingCSharp/ExpressYourself/DateRange.cs
+++ FunctionalProgrammingCSharp/ExpressYourself/DateRange.cs
@@ -6,20 +6,16 @@ namespace ExpressYourself
 {
     public class DateRange
     {
-        private readonly DateTime _start;
-        
-        private readonly DateTime _end;
+        public DateTime Start { get; }
 
-        public DateTime Start { get { return _start; } }
-        
-        public DateTime End { get { return _end; } }
+        public DateTime End { get; }
 
         public DateRange(DateTime start, DateTime end)
         {
             if (start.CompareTo(end) >= 0) throw new Exception("End must occur after the start");
 
-            _start = start;
-            _end = end;
+            Start = start;
+            End = end;
         }
 
         public bool DateIsInRange(DateTime checkDate)
