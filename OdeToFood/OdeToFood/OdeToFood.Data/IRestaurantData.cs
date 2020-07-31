c80ceaa9f43121359014ab5c047902a6edd04555
te25son <te25son@gmail.com>
2020-07-31 08:08:31 +0200

    Add data project and restaurant data interface

---
+++ OdeToFood/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>
+            {
+                new Restaurant {Id = 1, Name = "Frodo's Pizza", Location = "Shire", Cuisine = CuisineType.Italian },
+                new Restaurant {Id = 2, Name = "Curry By Gimly", Location = "Mines of Moria", Cuisine = CuisineType.Indian },
+                new Restaurant {Id = 3, Name = "Merry and Pippin's Taco Stand", Location = "Shire", Cuisine = CuisineType.Mexican },
+                new Restaurant {Id = 4, Name = "Sauron's Sushi", Location = "Mordor", Cuisine = CuisineType.Japanese }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll() =>
+            restaurants.OrderBy(r => r.Name);
+    }
+}
