f27983f7f5c5ba2d222099bfab2d0f6614c56947
te25son <te25son@gmail.com>
2020-07-31 08:24:27 +0200

    Register data and display in razor pages

--- OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,23 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaurdantData;
 
+        public IEnumerable<Restaurant> Restaurants { get; set; }
         public string Message { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurdantData)
         {
             this.config = config;
+            this.restaurdantData = restaurdantData;
         }
 
         public void OnGet()
         {
             Message = config["Message"];
+            Restaurants = restaurdantData.GetAll();
         }
     }
 }
