1617ce92f8413ec5cd6c549bb4731770e5ef0ff3
te25son <te25son@gmail.com>
2020-08-23 13:37:56 +0200

    Add form to edit page

--- OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -11,18 +12,24 @@ namespace OdeToFood.Pages.Restaurants
 {
     public class EditModel : PageModel
     {
-        private readonly IRestaurantData RestaurantData;
+        private readonly IRestaurantData _restaurantData;
+        
+        private readonly IHtmlHelper _htmlHelper;
 
         public Restaurant Restaurant { get; set; }
 
-        public EditModel(IRestaurantData restaurantData)
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
-            RestaurantData = restaurantData;
+            _restaurantData = restaurantData;
+            _htmlHelper = htmlHelper;
         }
 
         public IActionResult OnGet(int restaurantId)
         {
-            Restaurant = RestaurantData.GetById(restaurantId);
+            Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
+            Restaurant = _restaurantData.GetById(restaurantId);
 
             if (Restaurant == null)
             {
