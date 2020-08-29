bd8556a93eeb748bc409bd81335cd15d9e7d8153
te25son <te25son@gmail.com>
2020-08-29 09:14:41 +0200

    Redirect post to detail page when valid

--- OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -46,6 +46,7 @@ namespace OdeToFood.Pages.Restaurants
             {
                 _restaurantData.Update(Restaurant);
                 _restaurantData.Commit();
+                return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
             }
 
             Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
