7267a9fc640038ca0dad5c792204b7011aaa01e9
te25son <te25son@gmail.com>
2020-11-19 11:48:19 +0100

    Add delete model

---
+++ OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IRestaurantData RestaurantData;
+
+        public Restaurant Restaurant { get; set; }
+
+        public DeleteModel(IRestaurantData restaurantData)
+        {
+            RestaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = RestaurantData.GetById(restaurantId);
+
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int restaurantId)
+        {
+            var restaurant = RestaurantData.Delete(restaurantId);
+            RestaurantData.Commit();
+
+            if (restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            TempData["Message"] = $"{restaurant.Name} successfully deleted.";
+            return RedirectToPage("./List");
+        }
+    }
+}
