df95d572b1fb0aa2fafc3f10f7399c6f78eb46c6
te25son <te25son@gmail.com>
2020-12-22 17:23:01 +0100

    Configure middleware endpoints

--- PieShop/PieShop/Startup.cs
+++ PieShop/PieShop/Startup.cs
@@ -28,14 +28,16 @@ namespace PieShop
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
