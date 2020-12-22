6da0b1c0a3ae6edf0aeb536bda011ba1fc93f7b2
te25son <te25son@gmail.com>
2020-12-22 17:17:18 +0100

    Set up services for working with MVC

--- PieShop/PieShop/Startup.cs
+++ PieShop/PieShop/Startup.cs
@@ -16,6 +16,8 @@ namespace PieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register framework services.
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
