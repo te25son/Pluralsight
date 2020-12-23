2341fddab929607e9c30fa06145f4d4d7c56e2e4
te25son <te25son@gmail.com>
2020-12-23 18:37:11 +0100

    Configure our mock data and repositories as services

--- PieShop/PieShop/Startup.cs
+++ PieShop/PieShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PieShop.Models;
 
 namespace PieShop
 {
@@ -18,6 +19,10 @@ namespace PieShop
         {
             // Register framework services.
             services.AddControllersWithViews();
+
+            // Register own services.
+            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
