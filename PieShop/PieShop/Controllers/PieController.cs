d73d02a6c775f6f56318d398a62566aea6bfcf85
te25son <te25son@gmail.com>
2020-12-24 10:11:56 +0100

    Add pie controller

---
+++ PieShop/PieShop/Controllers/PieController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_pieRepository.Pies);
+        }
+    }
+}
