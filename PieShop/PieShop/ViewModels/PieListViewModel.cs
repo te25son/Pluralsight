c8e762b777a69cc31db4ddc90d63d77d29c57208
te25son <te25son@gmail.com>
2020-12-26 16:22:09 +0100

    Add pie view model

---
+++ PieShop/PieShop/ViewModels/PieListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PieShop.Models;
+using System.Collections.Generic;
+
+namespace PieShop.ViewModels
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+
+        public string CurrentCategory { get; set; }
+    }
+}
