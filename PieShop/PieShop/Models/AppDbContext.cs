89284b4bb14d78100b8a00927c3682917f592ba6
te25son <te25son@gmail.com>
2020-12-28 19:47:10 +0100

    Add dbcontext bridge class

---
+++ PieShop/PieShop/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PieShop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Pie> Pies { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
