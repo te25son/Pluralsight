07d66599231e7a3b12260e5a92d0311ed84b6d32
te25son <te25son@gmail.com>
2020-12-29 08:24:53 +0100

    Add db context to startup

--- PieShop/PieShop/Startup.cs
+++ PieShop/PieShop/Startup.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PieShop.Models;
@@ -13,12 +14,22 @@ namespace PieShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             // Register framework services.
             services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
 
             // Register own services.
             services.AddScoped<IPieRepository, MockPieRepository>();
