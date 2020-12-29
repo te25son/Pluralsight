21830fe2010f043ab710035fe6d61796dc5dd9a9
te25son <te25son@gmail.com>
2020-12-29 08:39:15 +0100

    Create pie repository and category repository

---
+++ PieShop/PieShop/Models/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext context;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            context = appDbContext;
+        }
+
+        public IEnumerable<Category> Categories => context.Categories;
+    }
+}
---
+++ PieShop/PieShop/Models/PieRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext context;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            context = appDbContext;
+        }
+
+        public IEnumerable<Pie> Pies => context.Pies.Include(p => p.Category);
+
+        public IEnumerable<Pie> PiesOfTheWeek => Pies.Where(p => p.IsPieOfTheWeek);
+
+        public Pie GetPieById(int id) => context.Pies.FirstOrDefault(p => p.Id.Equals(id));
+    }
+}
