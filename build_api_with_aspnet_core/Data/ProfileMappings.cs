d81537ddaf8e56b4ef99e24b16810326d31615c6
te25son <te25son@gmail.com>
2021-01-11 09:23:12 +0100

    Consolidated profiles in single file

--- build_api_with_aspnet_core/Data/ProfileMappings.cs
+++ build_api_with_aspnet_core/Data/ProfileMappings.cs
@@ -1,17 +1,19 @@
 ﻿using AutoMapper;
 using CoreCodeCamp.Models;
 
-namespace CoreCodeCamp.Data.Profiles
+namespace CoreCodeCamp.Data
 {
-    public class CampProfile : Profile
+    public class ProfileMappings : Profile
     {
-        public CampProfile()
+        public ProfileMappings()
         {
             CreateMap<Camp, CampModel>()
                 .ForMember(
                     destinationMember: c => c.Venue,
                     memberOptions: o => o.MapFrom(m => m.Location.VenueName)
                 );
+            CreateMap<Talk, TalkModel>();
+            CreateMap<Speaker, SpeakerModel>();
         }
     }
 }
