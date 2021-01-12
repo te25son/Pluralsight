e4667a686b3acb888652c871316cfaf370086e63
te25son <te25son@gmail.com>
2021-01-12 19:01:29 +0100

    Add talk controller with single get method

--- build_api_with_aspnet_core/Controllers/AppController.cs
+++ build_api_with_aspnet_core/Controllers/AppController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CoreCodeCamp.Components;
 using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
@@ -19,5 +20,10 @@ namespace CoreCodeCamp.Controllers
         protected LinkGenerator LinkGenerator { get; private set; }
 
         protected static IMapper Mapper => Mapping.CreateMapper();
+
+        public ActionResult RequestDatabaseFailure()
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+        }
     }
 }
---
+++ build_api_with_aspnet_core/Controllers/TalksController.cs
@@ -0,0 +1,34 @@
+﻿using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    [Route("api/camps/{moniker}/talks")]
+    public class TalksController : AppController
+    {
+        public TalksController(ICampRepository repository, LinkGenerator linkGenerator)
+            : base(repository, linkGenerator)
+        {
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker)
+        {
+            try
+            {
+                var talks = await Repository.GetTalksByMonikerAsync(moniker);
+
+                return Mapper.Map<TalkModel[]>(talks);
+            }
+            catch (Exception)
+            {
+                return RequestDatabaseFailure();
+            }
+        }
+    }
+}
